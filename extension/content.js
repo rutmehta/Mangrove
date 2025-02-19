68e7bb24f10b1191d80ec8ade51228c70bba916c
Roy Houwayek <76702666+EternalV2@users.noreply.github.com>
2025-02-19 17:04:48 -0500

    Delete extension directory

--- extension/content.js
+++
@@ -1,9 +0,0 @@
-fetch("http://127.0.0.1:5000/api/data")
-  .then(response => response.json())
-  .then(data => {
-    console.log("Received from backend:", data);
-  })
-  .catch(error => console.error("Error:", error));
-
-
- 
